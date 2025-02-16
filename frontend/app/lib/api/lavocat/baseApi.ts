29accec174602f96d25837e12abb9733792d24a6
leonardoaugusto <sleonardoaugusto@gmail.com>
2025-02-16 09:53:58 -0300

    contrib .env.local added

--- frontend/app/lib/api/lavocat/baseApi.ts
+++ frontend/app/lib/api/lavocat/baseApi.ts
@@ -1,9 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM4OTgzMDAxLCJqdGkiOiIwYWEzNzdlZTMzZTg0Zjg5YTFjZjFkNzk4MjNlYmQ5NiIsInVzZXJfaWQiOjF9.8PccIqqu6HVFRQC8-RT017bupjVCqXPpzsuSK8TV_ZY'
+const token = process.env.API_AUTH_TOKEN
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
