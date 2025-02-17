f0820af15a6ec922d9e7bf8cc314e476dbdb5de0
leonardoaugusto <sleonardoaugusto@gmail.com>
2025-02-16 21:42:17 -0300

    fix serve staticfiles

--- backend/lavocat/settings.py
+++ backend/lavocat/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'django_extensions',
     'corsheaders',
     'test_without_migrations',
@@ -51,7 +52,6 @@ INSTALLED_APPS = [
     'django_filters',
     'lavocat.core',
     'lavocat.attendances',
-    'storages',
 ]
 
 TEST_WITHOUT_MIGRATIONS_COMMAND = 'django_nose.management.commands.test.Command'
@@ -152,23 +152,26 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+USE_S3 = config('USE_S3', cast=bool, default=False)
 
 # Prod/Dev environment
-if config('USE_S3', cast=bool, default=False):
-    # aws settings
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', '')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', '')
-    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', '')
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    # s3 static settings
-    STATICFILES_STORAGE = 'storage_backends.StaticStorage'
-    AWS_STATIC_LOCATION = 'static'
-    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
-    STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = config("MEDIA_ROOT", default=Path.joinpath(BASE_DIR, "media"))
+if USE_S3:
+    # AWS S3 settings
+    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
+    AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", "")
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+
+    # Custom Static Storage
+    STATICFILES_STORAGE = "backend.storage_backends.StaticStorage"
+    AWS_STATIC_LOCATION = "static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/"
+
+    # Media remains local (if needed)
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = config("MEDIA_ROOT", default=Path(BASE_DIR, "media"))
 else:
     STATIC_URL = '/staticfiles/'
     STATIC_ROOT = str(Path.joinpath(BASE_DIR, 'staticfiles'))
