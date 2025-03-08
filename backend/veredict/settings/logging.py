acd5531e6e59a47d66df6d9cedf366e4ad2f1a4b
leonardoaugusto <sleonardoaugusto@gmail.com>
2025-03-08 11:22:42 -0300

    test sentry exception handling

--- backend/veredict/settings/logging.py
+++ backend/veredict/settings/logging.py
@@ -3,8 +3,7 @@ from decouple import config
 from django.conf import settings
 
 # AWS CloudWatch settings
-AWS_LOG_GROUP = "django-app"
-AWS_LOG_STREAM = "production"
+AWS_LOG_GROUP = "production"
 
 # Environment variable to enable/disable CloudWatch logging
 ENABLE_CLOUDWATCH_LOGS = config("WATCHTOWER_LOGS", default=False, cast=bool)
@@ -24,7 +23,6 @@ if ENABLE_CLOUDWATCH_LOGS:
         "class": "watchtower.CloudWatchLogHandler",
         "boto3_client": boto3.client("logs", region_name=config("AWS_REGION")),
         "log_group": AWS_LOG_GROUP,
-        "log_stream_name": AWS_LOG_STREAM,
         "formatter": "verbose",
     }
 
