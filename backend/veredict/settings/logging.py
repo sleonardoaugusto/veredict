87b16a80f8bef06256f0d2288aace49b177ba422
leonardoaugusto <sleonardoaugusto@gmail.com>
2025-03-08 00:00:34 -0300

    configure cloudwatch logging

--- backend/veredict/settings/logging.py
+++ backend/veredict/settings/logging.py
@@ -1,5 +1,32 @@
+import boto3
+from decouple import config
 from django.conf import settings
 
+# AWS CloudWatch settings
+AWS_LOG_GROUP = "django-app"
+AWS_LOG_STREAM = "production"
+
+# Environment variable to enable/disable CloudWatch logging
+ENABLE_CLOUDWATCH_LOGS = config("WATCHTOWER_LOGS", default=False, cast=bool)
+
+# Define handlers based on the environment variable
+handlers = {
+    "console": {
+        "level": "DEBUG",
+        "class": "logging.StreamHandler",
+        "formatter": "verbose",
+    },
+}
+
+if ENABLE_CLOUDWATCH_LOGS:
+    handlers["cloudwatch"] = {
+        "level": "INFO",
+        "class": "watchtower.CloudWatchLogHandler",
+        "boto3_client": boto3.client("logs", region_name=config("AWS_REGION")),
+        "log_group": AWS_LOG_GROUP,
+        "log_stream_name": AWS_LOG_STREAM,
+        "formatter": "verbose",
+    }
 
 LOGGING = {
     "version": 1,
@@ -11,17 +38,12 @@ LOGGING = {
             "style": "{",
         },
     },
-    "handlers": {
-        # Logs to the console (useful for development)
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-    },
+    "handlers": handlers,
     "loggers": {
         "veredict": {
-            "handlers": ["console"],
+            "handlers": ["console", "cloudwatch"]
+            if ENABLE_CLOUDWATCH_LOGS
+            else ["console"],
             "level": "DEBUG" if settings.DEBUG else "INFO",
             "propagate": False,
         },
