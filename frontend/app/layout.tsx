84727a87d4165b30dfe9e1450061312ab8e1a34a
leonardoaugusto <sleonardoaugusto@gmail.com>
2025-03-15 20:10:01 -0300

    daisyUI added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className={`${inter.className} antialiased`}>
         <ClientWrapper>{children}</ClientWrapper>
       </body>
