97b7f07dd2d9514870044967e2ec75f483b2001b
leonardoaugusto <sleonardoaugusto@gmail.com>
2025-05-12 09:29:07 -0300

    fix component

--- frontend/app/(pages)/appointments/page.tsx
+++ frontend/app/(pages)/appointments/page.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import AppointmentsGrid from './components/AppointmentsGrid'
 import Header from '@/app/ui/Header'
 import React from 'react'
+import Button from '@/app/ui/Button'
 
-export default async function Page() {
+export default function Page() {
   return (
     <div>
       <Header title="Atendimentos" />
+      <Button className="mt-4 mb-2 btn btn-sm join-item" onClick={() => {}}>
+        Novo Atendimento
+      </Button>
       <AppointmentsGrid />
     </div>
   )
