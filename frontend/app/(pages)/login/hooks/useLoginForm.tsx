e20cd6f216d708da5c1ac23003518ec792d86407
leonardoaugusto <sleonardoaugusto@gmail.com>
2025-05-15 21:43:39 -0300

    refactor login page

---
+++ frontend/app/(pages)/login/hooks/useLoginForm.tsx
@@ -0,0 +1,31 @@
+import * as Yup from 'yup'
+import { FormikValues } from 'formik'
+import { AuthService } from '@/app/lib/auth'
+import { useAuthenticateMutation } from '@/app/lib/api/lavocat/auth'
+import { useRouter } from 'next/navigation'
+
+export default function useLoginForm() {
+  const router = useRouter()
+
+  const [authenticate] = useAuthenticateMutation()
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Usuário é obrigatírio.'),
+    password: Yup.string().required('Senha é obrigatíria.'),
+  })
+
+  const handleSubmit = async (
+    values: FormikValues,
+    { setSubmitting }: { setSubmitting: (flag: boolean) => void }
+  ) => {
+    const resp = await authenticate({
+      username: values.username,
+      password: values.password,
+    }).unwrap()
+    AuthService.setToken(resp['access'])
+    setSubmitting(false)
+    router.push('/processings')
+  }
+
+  return { validationSchema, handleSubmit }
+}
